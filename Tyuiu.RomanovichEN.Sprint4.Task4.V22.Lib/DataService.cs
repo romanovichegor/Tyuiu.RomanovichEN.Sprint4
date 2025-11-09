3726401f310f8e597d34260906556eb980c18453
Егор Романович <Egor_rom13@mail.ru>
2025-11-09 10:53:43 +0500

    Task4

---
+++ Tyuiu.RomanovichEN.Sprint4.Task4.V22.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.RomanovichEN.Sprint4.Task4.V22.Lib
+{
+    public class DataService : ISprint4Task4V22
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] % 2 !=0)
+                        matrix[i, j] = 0;
+                }
+            }
+            return matrix;
+        }
+    }
+}
---
+++ Tyuiu.RomanovichEN.Sprint4.Task4.V22/Program.cs
@@ -0,0 +1,49 @@
+﻿using Tyuiu.RomanovichEN.Sprint4.Task4.V22.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = " Спринт #4 | Выполнил: Романович Е. Н. | ПКТб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Двумерные массивы(ввод с клавиатуры)                              *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #22                                                             *");
+        Console.WriteLine("* Выполнил: Романович Егор Николаевич | ПКТб-25-1                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        int rows,columns;
+        Console.WriteLine("Количество строк массива:");
+        rows = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Количество столбцов массива:");
+        columns = Convert.ToInt32(Console.ReadLine());
+        int[,] array = new int[rows,columns];
+        Console.WriteLine("***************************************************************************");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.WriteLine("Введите " + i + "," + j + " элемент массива");
+                array[i,j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        array = ds.Calculate(array);
+        for (int i = 0; i < rows; i ++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"{array[i, j]} \t ");
+            }
+            Console.WriteLine();
+        }
+        Console.Write(ds.Calculate(array));
+        Console.ReadKey();
+    }
+}
